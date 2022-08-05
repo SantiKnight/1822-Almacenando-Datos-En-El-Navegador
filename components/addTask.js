8fd0d4b914eb016826944e0ff6aaa5ab6af6856d
Santi Knight <santiknight2003@gmail.com>
2022-08-05 14:48:35 -0300

    clase05 y proyecto terminados

--- components/addTask.js
+++ components/addTask.js
@@ -1,3 +1,4 @@
+import { uniqueDates } from '../services/date.js';
 import checkComplete from './checkComplete.js';
 import deleteIcon from './deleteIcon.js';
 import { readTask } from './readTask.js';
@@ -19,9 +20,13 @@ export const addTask = (evento) =>{
     input.value = '';
     calendar.value = '';
 
+    const complete = false;
+
     const taskObj = {
         value,
         dateFormat,
+        complete,
+        id: uuid.v4(),
     };
 
     list.innerHTML = '';
@@ -33,23 +38,30 @@ export const addTask = (evento) =>{
     readTask();
 };
 
- export const createTask = ({value, dateFormat}) => {
+ export const createTask = ({value, dateFormat, complete, id}) => {
     const task = document.createElement('li');
             task.classList.add('card'); 
 
     const taskContent = document.createElement('div');
 
+    const check = checkComplete(id);
+
+    if(complete){
+        check.classList.toggle('fas');
+        check.classList.toggle('completeIcon');
+        check.classList.toggle('far');
+    }
+
     const titleTask = document.createElement('span');
             titleTask.classList.add('task');
             titleTask.innerText = value;
-            taskContent.appendChild(checkComplete());
+            taskContent.appendChild(check);
             taskContent.appendChild(titleTask);
 
     const dateElement = document.createElement("span");
             dateElement.innerHTML = dateFormat;
             task.appendChild(taskContent);
-            task.appendChild(dateElement);
-            task.appendChild(deleteIcon());
+            task.appendChild(deleteIcon(id));
 
     return task;
 };
